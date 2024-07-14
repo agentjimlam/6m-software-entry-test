ab93d4d6b8defa5d83ccb0cc2b17a85db3f81f52
Jimmy Lam <165074052+agentjimlam@users.noreply.github.com>
2024-07-15 00:52:29 +0800

    Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -12,25 +12,7 @@
 */
 
 function swap(x, y){
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        return -1;
-    }
-    
-    // Swap values
-    x = x + y;
-    y = x - y;
-    x = x - y;
-    
-    console.log("Swapped values: x =", x, ", y =", y);
-    return [x, y];
+    // Task 1: Add code here
 }
 
 // Task 2: Add code here
-
-
-let result1 = swap("Apple", 10);
-console.log(result1);  // Output: -1
-
-let result2 = swap(9, 17);
-console.log(result2);  // Output: Swapped values: x = 17 , y = 9
-                        //         [17, 9]
