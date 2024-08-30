27fd261fead64e6ad1b03f0b20c5257fde5b1839
Jimmy Lam <165074052+agentjimlam@users.noreply.github.com>
2024-08-30 23:23:15 +0800

    Answer q1.js

--- src/q1.js
+++ src/q1.js
@@ -13,6 +13,24 @@
 
 function swap(x, y){
     // Task 1: Add code here
+    if (typeof x == 'number' && typeof y == 'number'){          // if x is a number AND y is a number
+        x = x + y;  // x = 9 + 17 = 26
+        y = x - y;  // y = 26 - 17 = 9;
+        x = x - y;  // x = 26 - 9 = 17;
+
+        console.log('Swapped values, x = ' + x + ' y = ' + y);
+        return [x, y];        
+    }
+    else{
+        return -1;                                              // x and y are not numeric, return -1 
+    }
 }
 
 // Task 2: Add code here
+
+const scenarioA = swap('Apple', 10); // calls swap function with ('Apple', 10) as argument, stores returned value in  constant variable scenarioA
+console.log(scenarioA);
+
+
+const scenarioB = swap(9, 17); // calls swap function with (9, 17) as argument, stores returned value in constant variable scenarioB
+console.log(scenarioB);
